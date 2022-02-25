fdc322beb91d0db4daf1311a1cd28a3cadab091b
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-02-25 07:14:56 +0530

    Time: 1 ms (90.83%), Space: 41.9 MB (30.89%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,27 +1,24 @@
-class Solution {
-    public int compareVersion(String version1, String version2) {
-        int i = 0, j = 0;
-        
-        int value1, value2;
-        
-        while(i < version1.length() || j < version2.length()) {
-            
-            value1 = value2 = 0;
-            while(i < version1.length() && version1.charAt(i) != '.') {
-                value1 = value1 * 10 + (version1.charAt(i++) - '0');
+class Solution 
+{
+    public int compareVersion(String version1, String version2) 
+    {
+        String[] nums1 = version1.split("\\.");
+        String[] nums2 = version2.split("\\.");
+        int index = 0;    
+        while (index < Math.max(nums1.length, nums2.length)) 
+        {
+            int digit1 = index < nums1.length ? Integer.parseInt(nums1[index]) : 0;
+            int digit2 = index < nums2.length ? Integer.parseInt(nums2[index]) : 0;
+            if (digit1 > digit2) 
+            {
+                return 1;
             }
-            
-            while(j < version2.length() && version2.charAt(j) != '.') {
-                value2 = value2 * 10 + (version2.charAt(j++) - '0');
+            if (digit2 > digit1) 
+            {
+                return -1;
             }
-            
-            if(value1 != value2)
-                return Integer.compare(value1, value2);
-            
-            i++;
-            j++;
+            index++;
         }
-        
         return 0;
     }
 }
