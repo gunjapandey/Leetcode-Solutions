82a745fc0adaeb9e6608de80bf5ec54072acd054
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-03-10 19:47:26 +0530

    Time: 2 ms (83.65%), Space: 48.7 MB (5.43%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,57 +9,29 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-         ListNode temp1 = l1;
-        ListNode temp2 = l2;
-        int size1 = 0;
-        int size2 = 0;
-        while(temp1 != null){
-            temp1= temp1.next;
-            size1++;
-        }
-        while(temp2 != null){
-            temp2= temp2.next;
-            size2++;
-        }
-        ListNode n1;
-        ListNode n2;
-        ListNode root = null;
-        if(size1>size2){
-            n1 = l1;
-            n2 = l2;
-        }else{
-            n1 = l2;
-            n2 = l1;
-        }
-        int carry = 0;
-        while(n1 != null){
-            
-            if(n2 != null){
-                n1.val = n1.val + n2.val + carry;
-                n2 = n2.next;
-            }else{
-                n1.val = n1.val + carry;
-            }
-            carry = 0;
-            if(n1.val > 9){
-                int x = n1.val;
-                n1.val = x%10;
-                carry = x/10;
-            }
-            root = n1;
-            n1 = n1.next;
-        }
-        if(carry!= 0){
-            
-            ListNode l = new ListNode(carry);
-            root.next = l;
-        }
-        
-        if(size1>size2){
-            return l1;
-        }else{
-            return l2;
-        }
-    }
+   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+		ListNode temp = dummy;
+		int carry = 0;
+		while (l1 != null && l2 != null) {
+			int res = l1.val + l2.val + carry;
+			carry = res / 10;
+			temp.next = new ListNode(res % 10);
+			temp = temp.next;
+			l1 = l1.next;
+			l2 = l2.next;
+		}
+		l1 = l1 == null ? l2 : l1;
+		while (l1 != null) {
+			int res = l1.val + carry;
+			carry = res / 10;
+			temp.next = new ListNode(res % 10);
+			l1 = l1.next;
+			temp = temp.next;
+		}
+		if (carry == 1) {
+			temp.next = new ListNode(1);
+		}
+		return dummy.next;
+	}
 }
