18bd50dfa854111cf543d393f804a00a88b569c6
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-05-27 11:31:03 +0530

    Time: 11 ms (14.89%), Space: 43.1 MB (57.00%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st=new Stack<>();
+        st.push(-1);
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            
+            if(c=='(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if(st.isEmpty())
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    int len=i-st.peek();
+                    max=Math.max(max,len);
+                }
+                
+            }
+            
+        }
+        return max;
+    }
+}
