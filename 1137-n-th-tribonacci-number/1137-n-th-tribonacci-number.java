7b3a06c6c75361e9f4c4cb11d870117f4de68c21
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-06-06 15:48:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (44.33%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -9,8 +9,8 @@ class Solution {
         if(n==1)
             return 1;
         dp[2]=1;
-        if(n<=2)
-            return dp[n];
+        if(n==2)
+            return 1;
         for(int i=3;i<n+1;i++)
         {
             dp[i]=dp[i-3]+dp[i-1]+dp[i-2];
