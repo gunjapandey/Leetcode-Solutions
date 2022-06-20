cf4193477dba0f823aa0dfb9b3ab5ee9b25823a0
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-06-20 10:40:44 +0530

    Time: 4 ms (47.24%), Space: 49.2 MB (64.58%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int begin=0,end=0,farthest=0;
+        int jump=0;
+        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            farthest=Math.max(farthest,i+nums[i]);
+            
+            if(i==end)
+            {
+                jump++;
+                end=farthest;
+            }
+        }
+        return jump;
+    }
+}
