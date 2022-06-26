d4af125f92144a15dd6b942e0ff0ca07a5ffe4ce
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-06-26 10:01:08 +0530

    Time: 1 ms (100.00%), Space: 48.7 MB (18.29%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> ar=new ArrayList<String>();
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 && i%5==0)
+                ar.add("FizzBuzz");
+            else if(i%3==0)
+                ar.add("Fizz");
+            else if(i%5==0)
+                ar.add("Buzz");
+            else 
+                ar.add(Integer.toString(i));
+            
+        }
+        return ar;
+    }
+}
