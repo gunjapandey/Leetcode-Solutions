30a7e2cc374958cc843e16cf5eac5b7d1120f597
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-07-14 07:58:18 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.java
+++ Maximum Index - GFG/maximum-index.java
@@ -27,7 +27,7 @@ class Solution {
         // code here
         int diff=0,maxdiff=0;
       
-      for(int i=0;i<n/2;i++){
+      for(int i=0;i<n;i++){
           for(int j=n-1;j>=i;j--){
               if(arr[i]<=arr[j])
               {
