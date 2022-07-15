80d68c008838d85d09a460eb4f8ef688c5e218c4
Gunja Pandey <64955745+gunjapandey@users.noreply.github.com>
2022-07-15 09:55:37 +0530

    Added solution - LeetHub

---
+++ Inorder Traversal (Iterative) - GFG/inorder-traversal-iterative.java
@@ -0,0 +1,148 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+  
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	Solution g = new Solution();
+                ArrayList<Integer> res = g.inOrder(root);
+                for (int i = 0; i < res.size (); i++)
+                    System.out.print (res.get (i) + " ");
+                System.out.print("\n");
+                t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node 
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution
+{
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root)
+    {
+        // Code
+        ArrayList<Integer> ar=new ArrayList<Integer>();
+        // create an empty stack
+		Stack<Node> stack = new Stack<>();
+	
+		// start from the root node (set current node to the root node)
+		Node curr = root;
+	
+		// if the current node is null and the stack is also empty, we are done
+		while (!stack.empty() || curr != null)
+		{
+			// if the current node exists, push it into the stack (defer it)
+			// and move to its left child
+			if (curr != null)
+			{
+				stack.push(curr);
+				curr = curr.left;
+			}
+			else {
+				// otherwise, if the current node is null, pop an element from
+				// the stack, print it, and finally set the current node to its
+				// right child
+				curr = stack.pop();
+				ar.add(curr.data);
+	
+				curr = curr.right;
+			}
+		}
+		return ar;
+    }
+}
